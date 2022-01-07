2286ae671cfc536a02dd33f3a66593174b3491b7
Daniel_Alvarado <danielalvarado1498@gmail.com>
2022-01-06 18:07:24 -0600

    Converter array attempr

--- src/components/CurrencyColumn.js
+++ src/components/CurrencyColumn.js
@@ -1,17 +1,31 @@
 import React from "react";
 
-export default function CurrencyCloumn() {
+export default function CurrencyCloumn({
+  selectedCurrency,
+  onChangeCurrency,
+  amount,
+  onChangeAmount,
+}) {
   return (
     <div className="col-6">
-      <input type="number" className="inputSelector col-5" />
-      <select className="moneySelector">
+      <select
+        className="moneySelector"
+        value={selectedCurrency}
+        onChange={onChangeCurrency}
+      >
         <option value="">---</option>
         <option value="dolar">Dollar</option>
         <option value="euro">Euro</option>
-        <option value="mexican peso">Mexican Peso</option>
+        <option value="peso">Mexican Peso</option>
         <option value="quetzal">Quetzal</option>
         <option value="sol">Sol</option>
       </select>
+      <input
+        type="number"
+        className="inputSelector col-5"
+        value={amount}
+        onChange={onChangeAmount}
+      />
     </div>
   );
 }
--- src/components/CurrencySelector.js
+++ src/components/CurrencySelector.js
@@ -1,13 +1,105 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import CurrencyColumn from "./CurrencyColumn";
 
 function CurrencySelector() {
-  const [Currency, setCurrency] = useState("");
+  const [fromCurrency, setFromCurrency] = useState("");
+  const [toCurrency, setToCurrency] = useState("");
+  const [rate, setRate] = useState();
+  const [amount, setAmount] = useState();
+  const [amountOnFrom, setAmountOnFrom] = useState(true);
 
-  const handleSelect = (event) => setCurrency(event.target.value);
+  // The amountOnFrom variable determines if the user is typing the amount they want to convert on the
+  // "From:" input (this way the amountOnfrom is true) or the "To: " input (the amountOnFrom is false)
+
+  let toAmount, fromAmount;
+
+  if (amountOnFrom) {
+    fromAmount = amount;
+    toAmount = amount * rate;
+  } else {
+    toAmount = amount;
+    fromAmount = amount / rate;
+  }
+
+  // The converter array has the exchange rates for the selected countries
+
+  const converter = [
+    {
+      country: "peso",
+      rate: { peso: 1, dolar: 0.049, euro: 0.043, sol: 0.19, quetzal: 0.38 },
+    },
+    {
+      country: "dolar",
+      rate: { dolar: 1, euro: 0.88, sol: 3.96, quetzal: 7.72, peso: 20.59 },
+    },
+    {
+      country: "euro",
+      rate: { dolar: 1.13, euro: 1, sol: 4.48, quetzal: 8.74, peso: 23.3 },
+    },
+    {
+      country: "sol",
+      rate: { dolar: 0.25, euro: 0.22, sol: 1, quetzal: 1.95, peso: 5.2 },
+    },
+    {
+      country: "quetzal",
+      rate: { dolar: 0.13, euro: 0.11, sol: 0.51, quetzal: 1, peso: 2.67 },
+    },
+  ];
+
+  // This function will search the desired value on the converter array:
+
+  let searchRate = (fromCurrency, toCurrency) => {
+    let selectedArray = [];
+    let selectedRate = 1;
+
+    converter.forEach((item) => {
+      if (item.country == fromCurrency) {
+        selectedRate = item.rate[toCurrency];
+        item.rate.push(selectedArray);
+      }
+    });
+
+    console.log(selectedArray);
+
+    const clickedFromRate = converter.filter(
+      (coin) => coin.country == fromCurrency
+    );
+    return selectedArray;
+  };
+
+  console.log(searchRate("sol", "dolar"));
+
+  useEffect(() => {
+    let clickedCurrency = searchRate(fromCurrency);
+    setRate((converter.rate = 123123));
+  }, [fromCurrency, toCurrency]);
+
+  const handleFromAmount = (e) => {
+    setAmount(e.target.value);
+    setAmountOnFrom(true);
+  };
+
+  const handleToAmount = (e) => {
+    setAmount(e.target.value);
+    setAmountOnFrom(false);
+  };
+
   return (
     <>
-      <CurrencyColumn />
+      <span className="FromToText">From: </span>
+      <CurrencyColumn
+        selectedCurrency={fromCurrency}
+        onChangeCurrency={(event) => setFromCurrency(event.target.value)}
+        amount={fromAmount}
+        onChangeAmount={handleFromAmount}
+      />
+      <span className="FromToText"> To: </span>
+      <CurrencyColumn
+        selectedCurrency={toCurrency}
+        onChangeCurrency={(event) => setToCurrency(event.target.value)}
+        amount={toAmount}
+        onChangeAmount={handleToAmount}
+      />
     </>
   );
 }
